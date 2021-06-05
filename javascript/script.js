1ceabee994d88e7aa237fa00732a0b1edacbb548
sahil dholpuria <dholpuria1999@gmail.com>
2021-06-05 15:02:13 +0530

    comit

--- javascript/script.js
+++ javascript/script.js
@@ -2,8 +2,6 @@
         location.href = "https://sahildholpuria.tech";
     };
 
-    document.getElementById("contact").onclick = function () {
-        location.href = "https://github.com/Sahildholpuria/MVFlix-Web/blob/master/contactus.html";
-    };
+    
 
 
